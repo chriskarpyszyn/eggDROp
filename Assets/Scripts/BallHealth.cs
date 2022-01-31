f8eb7f76aafd5de2a8b01cfda8d0f026aba69634
Chris Karpyszyn <ckarpyszyn@tractionrec.com>
2022-01-31 09:52:51 -0500

    add respawn when falling

---
+++ Assets/Scripts/BallHealth.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallHealth : MonoBehaviour
+{
+
+    public int maxFallDistance = -10;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= maxFallDistance)
+        {
+            //todo-ck add checkpoints
+            SceneManager.LoadScene("Level01");
+            
+        }
+    }
+}
