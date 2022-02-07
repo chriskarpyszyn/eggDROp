857b48728f6eadaaa0b544a85f9af181b74246bd
Chris Karpyszyn <ckarpyszyn@tractionrec.com>
2022-02-06 19:41:53 -0500

    add jump and game over sounds

--- Assets/Scripts/BallHealth.cs
+++ Assets/Scripts/BallHealth.cs
@@ -1,20 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class BallHealth : MonoBehaviour
 {
 
     public int maxFallDistance = -10;
+    private bool isRestarting = false;
+
+    private AudioSource audioSource;
+    private AudioClip audioClip;
    
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y <= maxFallDistance)
+        if (transform.position.y <= maxFallDistance && !isRestarting)
         {
             //todo-ck add checkpoints
-            SceneManager.LoadScene("Level01");
+            StartCoroutine(RestartLevel());
+            
         }
+    }
+
+    private IEnumerator RestartLevel()
+    {
+        isRestarting = true;
+        audioSource = GetComponent<AudioSource>();
+        audioClip = audioSource.clip;
+        audioSource.Play();
+        yield return new WaitForSeconds(audioClip.length+(float)0.5);
+        SceneManager.LoadScene("Level01");
     }
 }
